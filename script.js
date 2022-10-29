16bbbb89e148ef54ec44411bfab26807adaa6e78
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2022-10-29 07:57:28 +0400

    feat: finish week one

---
+++ script.js
@@ -0,0 +1,43 @@
+const logoHeader = document.querySelectorAll(".header__logo");
+const navHeader = document.querySelector(".header__navigation--ul");
+const navFooter = document.querySelector(".footer__navigation--ul");
+
+navHeader.addEventListener("click", (e) => {
+    e.preventDefault();
+    for (const iterator of e.currentTarget.children) {
+        console.log(iterator);
+        iterator.children[0].classList .contains("highlight")? iterator.children[0].classList.remove("highlight"):"do nothing"
+    }
+    if (e.target.textContent =="About") {
+        window.location.href ="./index.html"
+    } else if (e.target.textContent =="Map") {
+        
+    } else if (e.target.textContent == "Zoos") {
+        
+    } else if (e.target.textContent == "Donate") {
+        window.location.href ="donate/index.html"
+    }
+    e.target.classList .add("highlight")
+})
+logoHeader.forEach((e) => {
+    e.addEventListener("click", () => {
+        window.location.href ="./index.html"
+    });
+})
+navFooter.addEventListener("click", (e) => {
+    e.preventDefault();
+    for (const iterator of e.currentTarget.children) {
+        console.log(iterator);
+        iterator.children[0].classList .contains("highlight")? iterator.children[0].classList.remove("highlight"):"do nothing"
+    }
+    if (e.target.textContent =="About") {
+        window.location.href ="./index.html"
+    } else if (e.target.textContent =="Map") {
+        
+    } else if (e.target.textContent == "Zoos") {
+        
+    } else if (e.target.textContent == "Donate") {
+        window.location.href ="donate/index.html"
+    }
+    e.target.classList .add("highlight")
+})
