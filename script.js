fd5fbdcea92459400c0153b73f4cad2c2f0f640a
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2022-11-27 21:57:33 +0400

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const logoHeader = document.querySelectorAll(".header__logo");
+const logoHeader = document.querySelectorAll(".header__logo--text");
 const navHeader = document.querySelector(".header__navigation--ul");
+const nvigationHead = document.querySelector(".nav__logo__container");
 const navFooter = document.querySelector(".footer__navigation--ul");
+const burgerIcon = document.querySelector(".burger__container");
+const overly = document.querySelector(".overly--black");
+const body = document.querySelector("body");
+const closeBtnBurger = document.querySelector(".img__x--continer");
 //////////////////////// week1-2 ///////////////////////////////
+overly.style.height = `${body.getBoundingClientRect().height}px`;
+
+function overlyFunc() {
+    window.addEventListener("resize", () => {
+        overly.style.height = `${body.getBoundingClientRect().height}px`;
+    });
+}
 function navFunction(){
 
     navHeader.addEventListener("click", (e) => {
@@ -42,7 +54,28 @@ function navFunction(){
         e.target.classList .add("highlight")
     });
 };
+function burgerFun() {
+    burgerIcon.addEventListener("click", () => {
+        nvigationHead.style.display = "block";
+        overly.classList.remove("none")
+        overly.classList.add("block")
+        closeBtnBurger.classList.remove("none")
+        closeBtnBurger.classList.add("block")
+    });
+    overly.addEventListener("click", () => {
+        nvigationHead.style.display = "none";
+        overly.classList.remove("block")
+        overly.classList.add("none")
+    });
+    closeBtnBurger.addEventListener("click", () => {
+        nvigationHead.style.display = "none";
+        overly.classList.remove("block")
+        overly.classList.add("none")
+    })
+}
+overlyFunc();
 navFunction();
+burgerFun();
 //////////////////////// week1-2 ///////////////////////////////
 
 
